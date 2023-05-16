b1fec048207fe0cd7bb4f5fa0f37609c75e8ba2a
f3hint0la <fehintolatech@gmail.com>
2023-05-16 10:52:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Elementicon
+
+> A live Weather App that allows you to search by city using a weather API.
+> Live demo [here](https://elementicon.netlify.app/).
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Features](#features)
+- [Screenshots](#screenshots)
+- [Project Status](#project-status)
+- [Room for Improvement](#room-for-improvement)
+- [Contact](#contact)
+
+## General Information
+
+<ul><li>The goal of this project was to create a weather app that allows you to search for the weather of any city in the world.</li></ul>
+
+## Technologies Used
+
+<ul>
+  <li>HTML5</li>
+  <li>CSS3</li>
+  <li>JAVASCRIPT</li>
+  <li>Open Weather API</li>
+  <li>Responsive Web Development</li> </ul>
+
+## Features
+
+List the ready features here:
+
+<ul>
+  <li>Search engine ready for searching the weather in any city</li>
+  <li>Shows current date and time</li></ul>
+
+## Screenshots
+
+<img src="assets/sreenshot.png" alt="screenshot">
---
+++ script.js
@@ -0,0 +1,96 @@
+const query = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-button");
+const weatherImg = document.querySelector(".weather-img");
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+
+const api = {
+  key: "2b046d694c0134dc1ee772379a5e91ed",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+searchBtn.addEventListener("click", () => {
+  fetchWeather(query.value);
+});
+
+const fetchWeather = async (city) => {
+  const response = await fetch(
+    `${api.base}weather?q=${city}&units=metric&appid=${api.key}`
+  );
+
+  const data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML =
+    data.name + ", " + data.sys.country;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".description").innerHTML =
+    data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector("#wind").innerHTML = data.wind.speed + " km/h";
+
+  switch (data.weather[0].main) {
+    case "Clear":
+      weatherImg.src = "assets/clear.svg";
+      break;
+    case "Clouds":
+      weatherImg.src = "assets/cloudy.svg";
+      break;
+    case "Rain":
+      weatherImg.src = "assets/rain.svg";
+      break;
+    case "Mist":
+      weatherImg.src = "assets/clouds.svg";
+      break;
+    case "Snow":
+      weatherImg.src = "assets/snow.svg";
+      break;
+    case "Thunderstorm":
+      weatherImg.src = "assets/tstorms.svg";
+      break;
+    case "Fog":
+      weatherImg.src = "assets/fog.svg";
+      break;
+  }
+};
+
+function formatDate() {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const day = today.getDay();
+  const date = today.getDate();
+  const month = today.getMonth();
+
+  timeEl.innerHTML = `${hours}:${minutes}`;
+  dateEl.innerHTML = days[day] + ", " + date + " " + months[month];
+
+  setTimeout(formatDate, 1000);
+}
+
+formatDate();
